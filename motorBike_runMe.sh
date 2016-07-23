2a69026308efb5a5c67e8ce7dcb6684d896996b9
Taylor Newill <tanewill@microsoft.com>
2016-07-23 18:27:50 -0400

    Update motorBike_runMe.sh

--- motorBike_runMe.sh
+++ motorBike_runMe.sh
@@ -11,18 +11,12 @@ blockMesh
 decomposePar
 mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles snappyHexMesh -parallel -overwrite
 
-#- For non-parallel running
-#cp -r 0.org 0 > /dev/null 2>&1
-
 #- For parallel running
 ls -d processor* | xargs -I {} rm -rf ./{}/0
 ls -d processor* | xargs -I {} cp -r 0.org ./{}/0
 
-#runParallel patchSummary 6
 mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles patchSummary -parallel
-#runParallel potentialFoam 6
 mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles potentialFoam -parallel
-#runParallel $(getApplication) 6
 mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles simpleFoam -parallel
 
 reconstructParMesh -constant
