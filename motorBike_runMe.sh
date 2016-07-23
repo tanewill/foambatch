e051416453d26c16ce0db283a26540f097ab7e79
Taylor Newill <tanewill@microsoft.com>
2016-07-23 16:25:33 -0400

    Update motorBike_runMe.sh

--- motorBike_runMe.sh
+++ motorBike_runMe.sh
@@ -1,11 +1,4 @@
-#!/usr/bin/env bash
-decomposePar
-mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles simpleFoam -parallel
-
-reconstructPar -latestTime
-foamToVTK -latestTime
-mv VTK/* ../TOKEEP/
-# ----------------------------------------------------------------- end-of-file
+#!/usr/bin/env bash +x
 echo $WM_PROJECT_DIR
 
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
@@ -25,12 +18,16 @@ mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles snappyHexM
 ls -d processor* | xargs -I {} rm -rf ./{}/0
 ls -d processor* | xargs -I {} cp -r 0.org ./{}/0
 
-runParallel patchSummary 6
-runParallel potentialFoam 6
-runParallel $(getApplication) 6
+#runParallel patchSummary 6
+mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles patchSummary -parallel
+#runParallel potentialFoam 6
+mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles potentialFoam -parallel
+#runParallel $(getApplication) 6
+mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles simpleFoam -parallel
 
-runApplication reconstructParMesh -constant
-runApplication reconstructPar -latestTime
-
+reconstructParMesh -constant
+reconstructPar -latestTime
+foamToVTK -latestTime
+mv VTK/* ../TOKEEP/
 # ----------------------------------------------------------------- end-of-file
 
