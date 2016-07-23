354aa9543565a16e38a4f4ee8d01e178b2172a19
Taylor Newill <tanewill@microsoft.com>
2016-07-23 07:38:30 -0400

    Update airFoil_runme.sh

--- airFoil_runme.sh
+++ airFoil_runme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 decomposePar
 #simpleFoam
-mpirun -np 16 -ppn 8 --host $1 simpleFoam -parallel
+mpirun -np 16 -ppn 8 --host $1 -wdir $2/share/OpenFOAM/foamModelFiles simpleFoam -parallel
 
 reconstructPar -latestTime
 foamToVTK -latestTime
